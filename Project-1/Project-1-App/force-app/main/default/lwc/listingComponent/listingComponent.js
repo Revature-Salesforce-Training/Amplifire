6e1eaa02bebe4205b4b8189d0d8cd9cb0564dfe6
sean-rw <seanrwhitley@gmail.com>
2022-02-24 15:33:12 -0800

    More fields from apex method, Add record-edit-form base impl

--- Project-1/Project-1-App/force-app/main/default/lwc/listingComponent/listingComponent.js
+++ Project-1/Project-1-App/force-app/main/default/lwc/listingComponent/listingComponent.js
@@ -1,10 +1,45 @@
-import { LightningElement, wire } from 'lwc';
-import queryByName from "@salesforce/apex/ProductsController.queryByName";
+import { LightningElement, wire, api } from 'lwc';
+import getProducts from "@salesforce/apex/ProductsController.getProducts";
+
+const DATA_COLUMNS = [
+    { label: 'Title', fieldName: 'Name' },
+    { label: 'Author', fieldName: 'Author__c' },
+    { label: 'Genre', fieldName: 'Genre__c' },
+    { label: 'Page Count', fieldName: 'Page_Count__c' },
+    { label: 'Description', fieldName: 'Descriptive__c' },
+    { label: 'Stock', fieldName: 'Current_Stock__c' }
+];
 
 export default class ListingComponent extends LightningElement {
 
-    queryString = '';
+    objectApiName = 'Product2';
+    dataColumns = DATA_COLUMNS;     // Column labels for lightning-datatable
+    recordId;                       // For button text and edit form conditional rend
+    isShowingEditForm = false;      // Datatable -or- edit form
 
-    @wire(queryByName, { name: '$queryString' })
+    @wire(getProducts)
     listings;
+
+    @api get buttonLabel() {
+        if (this.recordId) {
+            return 'Edit'
+        } else if (this.isShowingEditForm) {
+            return 'Cancel';
+        } 
+
+        return 'Add';
+    }
+
+    getRecordId() {
+        if (this.recordId) {
+            return this.recordId;
+        }
+
+        return '';
+    }
+
+    handleNewRecordAction() {
+        this.isShowingEditForm = this.isShowingEditForm ? false : true;
+        console.log(this.isShowingEditForm);
+    }
 }
