a4918edec5343517d24c2ef73373f7d294826b7d
sean-rw <seanrwhitley@gmail.com>
2022-02-24 10:18:00 -0800

    Fix conditional rendering

--- Project-1/Project-1-App/force-app/main/default/lwc/app/app.js
+++ Project-1/Project-1-App/force-app/main/default/lwc/app/app.js
@@ -1,52 +1,91 @@
+/* eslint-disable eqeqeq */
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
 
-    constructor() {
-        super();
-
-        this.isShowingHomeView = true;
-        this.isShowingListingView = false;
-        this.isShowingChatView = false;
-        this.isShowingFaqView = false;
-        this.isShowingOrderView = false;
-    }
+    isShowingHomeView = true;
+    isShowingListingsView = false;
+    isShowingOrdersView = false;
+    isShowingChatView = false;
+    isShowingFaqView = false;
 
     // Navigation handler for header page switching
     handleNavigation(event) {
-        // Event detail passed is the innerText of the .nav-link 
-        this.viewSwitcherHelper(event.detail.toLowerCase());
-    }
 
-    // Helper to set all boolean values at once, deterministically
-    viewSwitcherHelper(viewString) {
-        console.log(viewString);
-        // Set all to false, and set true individually in switch statement
-        this.isShowingHomeView = false;
-        this.isShowingListingView = false;
-        this.isShowingChatView = false;
-        this.isShowingFaqView = false;
-        this.isShowingOrderView = false;
+        let component = event.detail.toLowerCase();
+    
+        console.log(component);
 
-        switch (viewString) {
-            case 'home':
-                this.isShowingHomeView = true;
-                break;
-            case 'listing':
-                this.isShowingListingView = true;
-                break;
-            case 'chat':
-                this.isShowingChatView = true;
-                break;
-            case 'f.a.q.':
-                this.isShowingFaqView = true;
-                break;
-            case 'orders':
-                this.isShowingOrderView = true;
-                break;
-            default:
-                this.isShowingHome = true;
-                break;
+        if (component == 'home') {
+            this.isShowingHomeView = true;
+            this.isShowingListingsView = false;
+            this.isShowingOrdersView = false;
+            this.isShowingChatView = false;
+            this.isShowingFaqView = false;
+        } else if (component == 'listings') {
+            console.log('Inside Listings')
+            this.isShowingHomeView = false;
+            this.isShowingListingsView = true;
+            this.isShowingOrdersView = false;
+            this.isShowingChatView = false;
+            this.isShowingFaqView = false;
+        } else if (component == 'order') {
+            this.isShowingHomeView = false;
+            this.isShowingListingsView = false;
+            this.isShowingOrdersView = true;
+            this.isShowingChatView = false;
+            this.isShowingFaqView = false;
+        } else if (component == 'chat') {
+            this.isShowingHomeView = false;
+            this.isShowingListingsView = false;
+            this.isShowingOrdersView = false;
+            this.isShowingChatView = true;
+            this.isShowingFaqView = false;
+        } else if (component == 'f.a.q.') {
+            this.isShowingHomeView = false;
+            this.isShowingListingsView = false;
+            this.isShowingOrdersView = false;
+            this.isShowingChatView = false;
+            this.isShowingFaqView = true;
         }
+        // switch (viewString) {
+        //     case 'home':
+        //         this.isShowingHomeView = true;
+        //         this.isShowingListingView = false;
+        //         this.isShowingOrderView = false;
+        //         this.isShowingChatView = false;
+        //         this.isShowingFaqView = false;
+        //         break;
+        //     case 'listing':
+        //         this.isShowingHomeView = false;
+        //         this.isShowingListingView = true;
+        //         this.isShowingOrderView = false;
+        //         this.isShowingChatView = false;
+        //         this.isShowingFaqView = false;
+        //         break;
+        //     case 'orders':
+        //         this.isShowingHomeView = false;
+        //         this.isShowingListingView = false;
+        //         this.isShowingOrderView = true;
+        //         this.isShowingChatView = false;
+        //         this.isShowingFaqView = false;
+        //         break;
+        //     case 'chat':
+        //         this.isShowingHomeView = false;
+        //         this.isShowingListingView = false;
+        //         this.isShowingOrderView = false;
+        //         this.isShowingChatView = true;
+        //         this.isShowingFaqView = false;
+        //         break;
+        //     case 'f.a.q.':
+        //         this.isShowingHomeView = false;
+        //         this.isShowingListingView = false;
+        //         this.isShowingOrderView = false;
+        //         this.isShowingChatView = false;
+        //         this.isShowingFaqView = true;
+        //         break;
+        //     default:
+        //         console.log('Unrecognized navigation');
+        // }
     }
 }
