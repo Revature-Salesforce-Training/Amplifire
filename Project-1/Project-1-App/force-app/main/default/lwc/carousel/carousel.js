926e5e74cac686a5f02b47a274f9e65a942e70f5
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2022-02-25 12:13:20 -0500

    Carousel edit (try)

--- Project-1/Project-1-App/force-app/main/default/lwc/carousel/carousel.js
+++ Project-1/Project-1-App/force-app/main/default/lwc/carousel/carousel.js
@@ -1,23 +1,27 @@
 import { LightningElement } from 'lwc';
-import book1 from '@salesforce/resourceUrl/book1';
-import book2 from '@salesforce/resourceUrl/book2';
-import book3 from '@salesforce/resourceUrl/book3';
+// import book1 from '@salesforce/resourceUrl/book1';
+// import book2 from '@salesforce/resourceUrl/book2';
+// import book3 from '@salesforce/resourceUrl/book3';
+
+// import book1 from '..\resources\book1.webp';
+// import book2 from '..\resources\book2.jpg';
+// import book3 from '..\resources\book3.jpg';
 
 
 export default class Carousel extends LightningElement {
-    book1Img = book1;
-    book2Img = book2;
-    book3Img = book3;
+    book1Img = "https://media.discordapp.net/attachments/942900074705453056/946785460359933983/book1.jpg?width=816&height=510";
+    book2Img = "https://media.discordapp.net/attachments/942900074705453056/946785460582236190/book2.jpg?width=898&height=505";
+    book3Img = "https://media.discordapp.net/attachments/942900074705453056/946785460796153856/book3.jpg?width=898&height=449";
 
 
 
-    handleEvent1(event) {
-        if (event.target.getAttribute('aria-hidden')) {
-            event.target.setAttribute('aria-hidden', false);
-        }  
-            event.target.setAttribute('aria-hidden', true);
+    // handleEvent1(event) {
+    //     if (event.target.getAttribute('aria-hidden')) {
+    //         event.target.setAttribute('aria-hidden', false);
+    //     }  
+    //         event.target.setAttribute('aria-hidden', true);
     
 
 
-    } 
+    // } 
 }
