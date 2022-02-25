5ceac96c4c5e9b420244997c4e94bcf59ab14251
Drew Williams <awilliams1789@gmail.com>
2022-02-25 03:19:35 -0600

    finalize

--- Project-1/Project-1-App/force-app/main/default/lwc/app/app.js
+++ Project-1/Project-1-App/force-app/main/default/lwc/app/app.js
@@ -6,7 +6,7 @@ export default class App extends LightningElement {
     isShowingHomeView = true;
     isShowingListingsView = false;
     isShowingOrdersView = false;
-    isShowingChatView = false;
+    isShowingCarouselView = false;
     isShowingFaqView = false;
 
     // Navigation handler for header page switching
@@ -20,72 +20,32 @@ export default class App extends LightningElement {
             this.isShowingHomeView = true;
             this.isShowingListingsView = false;
             this.isShowingOrdersView = false;
-            this.isShowingChatView = false;
+            this.isShowingCarouselView = false;
             this.isShowingFaqView = false;
         } else if (component == 'listings') {
-            console.log('Inside Listings')
             this.isShowingHomeView = false;
             this.isShowingListingsView = true;
             this.isShowingOrdersView = false;
-            this.isShowingChatView = false;
+            this.isShowingCarouselView = false;
             this.isShowingFaqView = false;
         } else if (component == 'orders') {
             this.isShowingHomeView = false;
             this.isShowingListingsView = false;
             this.isShowingOrdersView = true;
-            this.isShowingChatView = false;
+            this.isShowingCarouselView = false;
             this.isShowingFaqView = false;
-        } else if (component == 'chat') {
+        } else if (component == 'carousel') {
             this.isShowingHomeView = false;
             this.isShowingListingsView = false;
             this.isShowingOrdersView = false;
-            this.isShowingChatView = true;
+            this.isShowingCarouselView = true;
             this.isShowingFaqView = false;
         } else if (component == 'f.a.q.') {
             this.isShowingHomeView = false;
             this.isShowingListingsView = false;
             this.isShowingOrdersView = false;
-            this.isShowingChatView = false;
+            this.isShowingCarouselView = false;
             this.isShowingFaqView = true;
         }
-        // switch (viewString) {
-        //     case 'home':
-        //         this.isShowingHomeView = true;
-        //         this.isShowingListingView = false;
-        //         this.isShowingOrderView = false;
-        //         this.isShowingChatView = false;
-        //         this.isShowingFaqView = false;
-        //         break;
-        //     case 'listing':
-        //         this.isShowingHomeView = false;
-        //         this.isShowingListingView = true;
-        //         this.isShowingOrderView = false;
-        //         this.isShowingChatView = false;
-        //         this.isShowingFaqView = false;
-        //         break;
-        //     case 'orders':
-        //         this.isShowingHomeView = false;
-        //         this.isShowingListingView = false;
-        //         this.isShowingOrderView = true;
-        //         this.isShowingChatView = false;
-        //         this.isShowingFaqView = false;
-        //         break;
-        //     case 'chat':
-        //         this.isShowingHomeView = false;
-        //         this.isShowingListingView = false;
-        //         this.isShowingOrderView = false;
-        //         this.isShowingChatView = true;
-        //         this.isShowingFaqView = false;
-        //         break;
-        //     case 'f.a.q.':
-        //         this.isShowingHomeView = false;
-        //         this.isShowingListingView = false;
-        //         this.isShowingOrderView = false;
-        //         this.isShowingChatView = false;
-        //         this.isShowingFaqView = true;
-        //         break;
-        //     default:
-        //         console.log('Unrecognized navigation');
-        // }
     }
 }
