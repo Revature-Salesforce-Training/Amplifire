780f4ff003261ff80c678a3e58260b07a159687b
Drew Williams <awilliams1789@gmail.com>
2022-03-09 01:21:54 -0600

    finalizing components

--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
@@ -5,22 +5,6 @@
 */
 
 ({
-    //maps out the columns
-    init: function (component, event, helper) {
-        component.set('v.columns', [
-            {label: 'Account name', fieldName: 'Name', type: 'text'}, 
-            {label: 'Phone', fieldName: 'Phone', type: 'phone'},
-            {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
-        ]);
-
-        var action = component.get('c.TableController')
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if(state == 'SUCCESS') {
-                var responseValue = response.getReturnValue();
-                component.set('v.data', responseValue);
-            }
-        });
-        $A.enqueueAction(action);
-    }
+    init: function (component, event, helper) {helper.helperMethod(component);}
 })
+
--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
@@ -3,7 +3,24 @@
 *	Description:- Data table that displays the current Accounts
 *	Date: 3/8/22
 */
-
 ({
-    helperMethod : function() {}
+    helperMethod : function(component) 
+        {
+            component.set('v.columns', [
+                {label: 'Account name', fieldName: 'Name', type: 'text'}, 
+                {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+                {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
+            ]);
+    
+            var action = component.get('c.TableController')
+            action.setCallback(this, function(response){
+                var state = response.getState();
+                if(state == 'SUCCESS') {
+                    var responseValue = response.getReturnValue();
+                    component.set('v.data', responseValue);
+                }
+            });
+            $A.enqueueAction(action);
+        }
+    
 })
