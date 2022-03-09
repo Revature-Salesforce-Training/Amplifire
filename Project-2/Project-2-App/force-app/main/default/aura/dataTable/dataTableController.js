a4e01fcf19adc1403f2e70a15f0cb347596fe84c
Drew Williams <awilliams1789@gmail.com>
2022-03-09 09:50:11 -0600

    final

--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
@@ -3,22 +3,14 @@
 *	Description:- Data table that displays the current Accounts
 *	Date: 3/8/22
 */
-({
-    init : function(component, event, helper) {
-        component.set('v.columns', [
-        {label: 'Account name', fieldName: 'Name', type: 'text'}, 
-        {label: 'Phone', fieldName: 'Phone', type: 'phone'},
-        {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
-    ]);
-        var action = component.get('c.getAccount');
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if(state === 'SUCCESS') {
-                var responseValue = response.getReturnValue();
-                component.set('v.data', responseValue);
-            }
-        });
-        $A.enqueueAction(action);
-    }
-});
+    ({
+        init: function (cmp, event, helper) {
+             cmp.set('v.mycolumns', [
+                {label: 'Account name', fieldName: 'Name', type: 'text'}, 
+                {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+                {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
+             ]);
+             helper.getData(cmp);
+         }
+     })
 
--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
@@ -5,6 +5,20 @@
 */
 ({
     helperMethod : function(){}
-
+    ({
+        getData : function(cmp) {
+            var action = cmp.get('c.TableController');
+            action.setCallback(this, $A.getCallback(function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    cmp.set('v.mydata', response.getReturnValue());
+                } else if (state === "ERROR") {
+                    var errors = response.getError();
+                    console.error(errors);
+                }
+            }));
+            $A.enqueueAction(action);
+        }
+    })
     
 })
