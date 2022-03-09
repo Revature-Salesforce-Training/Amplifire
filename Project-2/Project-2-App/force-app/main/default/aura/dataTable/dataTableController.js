4be2cc6d9f05702d50699f741f133be86528b0a4
Drew Williams <awilliams1789@gmail.com>
2022-03-09 02:32:33 -0600

    table edits

--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableController.js
@@ -5,6 +5,21 @@
 */
 
 ({
-    init: function (component, event, helper) {helper.helperMethod(component);}
+    init : function(component, event, helper) {
+        component.set('v.columns', [
+        {label: 'Account name', fieldName: 'Name', type: 'text'}, 
+        {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+        {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
+    ]);
+        var action = component.get('c.getAccount');
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS') {
+                var responseValue = response.getReturnValue();
+                component.set('v.data', responseValue);
+            }
+        });
+        $A.enqueueAction(action);
+    }
 })
 
--- Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
+++ Project-2/Project-2-App/force-app/main/default/aura/dataTable/dataTableHelper.js
@@ -4,23 +4,7 @@
 *	Date: 3/8/22
 */
 ({
-    helperMethod : function(component) 
-        {
-            component.set('v.columns', [
-                {label: 'Account name', fieldName: 'Name', type: 'text'}, 
-                {label: 'Phone', fieldName: 'Phone', type: 'phone'},
-                {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_self'}},
-            ]);
-    
-            var action = component.get('c.TableController')
-            action.setCallback(this, function(response){
-                var state = response.getState();
-                if(state == 'SUCCESS') {
-                    var responseValue = response.getReturnValue();
-                    component.set('v.data', responseValue);
-                }
-            });
-            $A.enqueueAction(action);
-        }
+    helperMethod : function(){}
+
     
 })
